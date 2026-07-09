ecdeb6e2e394ce07a3e5a73e793a23a5b3370e96
Runlikey2 <105078313+Runlikey2@users.noreply.github.com>
2026-07-09 18:05:24 +0100

    Heres the shit roblox version-90f2fddd3b244ff6

---
+++ src/dllmain.cpp
@@ -0,0 +1,19 @@
+#include <Windows.h>
+#include <thread>
+
+constexpr uintptr_t CONSOLE_PRINT_OFFSET = 0x46d3410;
+using console_print_fn = void(__fastcall*)(void*, const char*);
+
+void init() {
+	uintptr_t base = reinterpret_cast<uintptr_t>(GetModuleHandleA(nullptr));
+	auto print = reinterpret_cast<console_print_fn>(base + CONSOLE_PRINT_OFFSET);
+	print(nullptr, "hello world");
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved) {
+	if (reason == DLL_PROCESS_ATTACH) {
+		DisableThreadLibraryCalls(hModule);
+		std::thread(init).detach();
+	}
+	return TRUE;
+}
